d37e99d6d9e459bee5bf676f515da95ae0f73bc9
denisandrejev6-hue <denisandrejev6@gmail.com>
2026-03-19 08:06:58 +0000

    da

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -306,6 +306,7 @@ button:hover, .btn:hover {
 </style>
 </head>
 <body>
+  // navigacijas josla
   <header>
     <div class="nav container" style="display: flex; justify-content: center; align-items: center; gap: 16px; flex-wrap: wrap;">
       @auth
@@ -319,7 +320,7 @@ button:hover, .btn:hover {
       @endauth
     </div>
 
-<!-- special top-right container for auth controls -->
+// kreisajā augšējā stūrī izveidojam kastīti, kurā parādās lietotāja vārds, uzvārds un loma un poga izrakstities\ielogoties
 <div class="auth-container">
   @if(auth()->check())
     <div style="display: flex; align-items: center; gap: 10px;">
