4ddcd26e028c41196afaba7425cc8584520d28a6
denisandrejev6-hue <denisandrejev6@gmail.com>
2026-03-19 09:19:59 +0000

    yhguh

--- resources/views/create.blade.php
+++ resources/views/create.blade.php
@@ -116,20 +116,30 @@
             </div>
 
             <!-- Darbinieks un Telpa -->
+
+            <!-- Darbinieks -->
             <div class="form-row" style="margin-bottom:32px;">
                 <div class="form-group">
                     <label>Darbinieks <span class="required-star">*</span></label>
-                    <select name="darbinieks_id"
+                    @if(auth()->user()->loma === 'Darbinieks')
+                        <input type="text" value="{{ auth()->user()->vards }}" class="form-control" disabled>
+                        <input type="hidden" name="darbinieks_id" value="{{ auth()->user()->ID }}">
+                        @else
+                      <select name="darbinieks_id"
                         class="form-control @error('darbinieks_id') is-invalid @enderror">
                         <option value="">-- Izvēlieties darbinieku --</option>
                         @foreach($darbinieki as $d)
                             <option value="{{ $d->ID }}" {{ old('darbinieks_id') == $d->ID ? 'selected' : '' }}>{{ $d->vards }}</option>
                         @endforeach
                     </select>
+                    @endif
+
                     @error('darbinieks_id')
                         <span class="invalid-feedback">{{ $message }}</span>
                     @enderror
                 </div>
+
+                <!-- Telpa -->
                 <div class="form-group">
                     <label>Telpa <span class="required-star">*</span></label>
                     <select name="telpa_id"
